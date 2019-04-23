d1a962f356bfb69121074c96796a23b4c4c2263a
BaiCai <709931138@qq.com>
2019-04-23 12:00:59 +0800

    新业务开发中

--- src/main/java/com/d2c/shop/modules/core/query/BankCardQuery.java
+++ src/main/java/com/d2c/shop/modules/core/query/BankCardQuery.java
@@ -12,13 +12,10 @@ import lombok.EqualsAndHashCode;
  */
 @Data
 @EqualsAndHashCode(callSuper = false)
-public class ShopWithdrawQuery extends BaseQuery {
+public class BankCardQuery extends BaseQuery {
 
     @Condition(condition = ConditionEnum.EQ)
     @ApiModelProperty(value = "店铺ID")
     private Long shopId;
-    @Condition(condition = ConditionEnum.EQ)
-    @ApiModelProperty(value = "状态")
-    private String status;
 
 }
---
+++ src/main/java/com/d2c/shop/modules/core/service/impl/BankCardServiceImpl.java
@@ -0,0 +1,15 @@
+package com.d2c.shop.modules.core.service.impl;
+
+import com.d2c.shop.common.api.base.BaseService;
+import com.d2c.shop.modules.core.mapper.BankCardMapper;
+import com.d2c.shop.modules.core.model.BankCardDO;
+import com.d2c.shop.modules.core.service.BankCardService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author BaiCai
+ */
+@Service
+public class BankCardServiceImpl extends BaseService<BankCardMapper, BankCardDO> implements BankCardService {
+
+}
